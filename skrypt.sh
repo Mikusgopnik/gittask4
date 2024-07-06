c6b8898e1194be16e06ce28e6663082231655234
Mikusgopnik <157295444+Mikusgopnik@users.noreply.github.com>
2024-07-06 15:37:35 +0200

    Update skrypt.sh
    dopisanie pozostałych funkcji

--- skrypt.sh
+++ skrypt.sh
@@ -1,9 +1,39 @@
 #!/bin/bash
 a=$1
+c=$2
+if [[ -z "$c" ]]
+then 
+y=100
+fi
 case "$a" in
-
-  "--date") date -u ;;
-
-  *) echo "blad" ;;
-
+  "--date" | "-d" ) date -u ;;
+  "--logs" | "-l" ) b=1
+  while [ $b -le $c ]
+do
+touch log"$b".txt
+echo log"$b".txt >> log"$b".txt
+echo "stworzony przez skrypt.sh w dniu o godzinie:" >> log"$b".txt
+date -u >> log"$b".txt
+b=$((b+1))
+done 
+;;
+  "--error" | "-e") b=1
+  while [ $b -le $c ]
+do
+mkdir error"$b"
+cd error"$b"
+touch error"$b".txt
+echo error"$b".txt >> error"$b".txt
+echo "stworzony przez skrypt.sh w dniu o godzinie:" >> error"$b".txt
+date -u >> error"$b".txt
+cd -
+b=$((b+1))
+done
+;;
+"--help" | "-h") echo "Dostepne komendy:"
+echo "--date | -d"
+echo "--logs | -l"
+echo "--help | -h"
+echo "--error | -e"
+  *) echo "funkcja nie została podana" ;;
 esac
